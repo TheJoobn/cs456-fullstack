28671fa88aa8bb266897d83d597d67cfef8b8797
Joobn <ja-knut@hotmail.com>
2025-07-25 18:38:22 -0600

    Module 4 completed - Seed Data and Model Folder

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -5,7 +5,7 @@ var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
 const travel = (req, res) => {
   res.render('travel', {
     title: 'Travlr Getaways',
-    trips: trips  
+    trips  
   });
 };
 
